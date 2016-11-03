73444820effd26e4870b3294741fac6509bc5982
Davakiin <1183209328@qq.com>
2016-11-03 14:16:56 +0800

    Update TimestampTransfer.java

--- ex1/TimestampTransfer.java
+++ ex1/TimestampTransfer.java
@@ -17,16 +17,17 @@ public class TimestampTransfer {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置inputformat时间的格式
+		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");//设置outputFormat时间的格式
 		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-			Date lineDate = null;
+			String line = scanner.nextLine();//定义变量line为输入的一行数据
+			Date lineDate = null;//定义时间戳变量
 			long lineTimestamp;
 			try {
-				lineDate = inputFormat.parse(line);
-				lineTimestamp = lineDate.getTime();
+				lineDate = inputFormat.parse(line);//将输入的line转换为时间戳
+				lineTimestamp = lineDate.getTime();//使用gettime()方法来计算1970年一月一日至今有多少时间并返回给linTimestamp
 				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);
+				//lineDate = line = 2016-11-11 11:11:11 
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
